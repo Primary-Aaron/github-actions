1d9fbdb184dc6dabae9e98ce01520140e049ae61
PrimaryAaron <>
2021-06-06 08:42:24 +0000

    Initial commit

---
+++ Github-Actions-CI-main/test.py
@@ -0,0 +1,15 @@
+import unittest
+import example
+
+
+class TestCase(unittest.TestCase):
+
+    def test_add_1(self):
+        self.assertEqual(example.add(1, 2), 3)
+
+    def test_subtract_1(self):
+        self.assertEqual(example.subtract(1, 1), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
